131f53cb656900e207e4ea59759b9c6876ff6a4c
ANNMAINAWANGARI <wangariann371@gmail.com>
2024-04-29 20:10:05 +0300

    hardhat config setup for deployment on arbitrum sepolia testnet

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity:{
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks:{
+    arbitrumSepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+  }
+  
 };
 
 export default config;
