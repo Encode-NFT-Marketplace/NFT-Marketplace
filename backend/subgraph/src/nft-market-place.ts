d97952c52f53076f585bd23baf72575a610f9ecd
anthonykimani <kimaniantoni@gmail.com>
2024-05-02 17:51:05 +0300

    setups subgraph

--- backend/subgraph/src/nft-market-place.ts
+++ backend/subgraph/src/nft-market-place.ts
@@ -1,53 +1,100 @@
 import {
-  ItemBought as ItemBoughtEvent,
-  ItemCanceled as ItemCanceledEvent,
-  ItemListed as ItemListedEvent
-} from "../generated/NFTMarketPlace/NFTMarketPlace"
-import { ItemBought, ItemCanceled, ItemListed } from "../generated/schema"
+  ItemCanceled as ListingCanceled,
+  ItemListed as ListingCreated,
+  ItemBought as ListingPurchased,
+} from "../generated/NFTMarketplace/NFTMarketplace";
+import { store } from "@graphprotocol/graph-ts";
+import { ListingEntity } from "../generated/schema";
 
-export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.buyer = event.params.buyer
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+export function handleListingCreated(event: ListingCreated): void {
+  // Create a unique ID that refers to this listing
+  // The NFT Contract Address + Token ID + Seller Address can be used to uniquely refer
+  // to a specific listing
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Create a new entity and assign it's ID
+  let listing = new ListingEntity(id);
 
-  entity.save()
+  // Set the properties of the listing, as defined in the schema,
+  // based on the event
+  listing.seller = event.params.seller;
+  listing.nftAddress = event.params.nftAddress;
+  listing.tokenId = event.params.tokenId;
+  listing.price = event.params.price;
+
+  // Save the listing to the nodes, so we can query it later
+  listing.save();
 }
 
-export function handleItemCanceled(event: ItemCanceledEvent): void {
-  let entity = new ItemCanceled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
+export function handleListingCanceled(event: ListingCanceled): void {
+  // Recreate the ID that refers to the listing
+  // Since the listing is being updated, the datastore must already have an entity with this ID
+  // from when the listing was first created
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString() +
+    "-" +
+    event.params.seller.toHex();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Load the listing to see if it exists
+  let listing = ListingEntity.load(id);
 
-  entity.save()
+  // If it does
+  if (listing) {
+    // Remove it from the store
+    store.remove("ListingEntity", id);
+  }
 }
 
-export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.seller = event.params.seller
-  entity.nftAddress = event.params.nftAddress
-  entity.tokenId = event.params.tokenId
-  entity.price = event.params.price
+export function handleListingPurchased(event: ListingPurchased): void {
+  // Recreate the ID that refers to the listing
+  // Since the listing is being updated, the datastore must already have an entity with this ID
+  // from when the listing was first created
+  const id =
+    event.params.nftAddress.toHex() +
+    "-" +
+    event.params.tokenId.toString();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Attempt to load a pre-existing entity, instead of creating a new one
+  let listing = ListingEntity.load(id);
 
-  entity.save()
+  // If it exists
+  if (listing) {
+    // Set the buyer
+    listing.buyer = event.params.buyer;
+
+    // Save the changes
+    listing.save();
+  }
 }
+
+// export function handleListingUpdated(event: ListingUpdated): void {
+//   // Recreate the ID that refers to the listing
+//   // Since the listing is being updated,
+//   // the datastore must already have an entity with this ID
+//   // from when the listing was first created
+//   const id =
+//     event.params.nftAddress.toHex() +
+//     "-" +
+//     event.params.tokenId.toString() +
+//     "-" +
+//     event.params.seller.toHex();
+
+//   // Attempt to load a pre-existing entity, instead of creating a new one
+//   let listing = ListingEntity.load(id);
+
+//   // If it exists
+//   if (listing) {
+//     // Update the price
+//     listing.price = event.params.newPrice;
+
+//     // Save the changes
+//     listing.save();
+//   }
+// }
